7133accc23630db91fd3c8e50962b30b5ccf897a
gabrielestacio <ogabrielestacio@gmail.com>
2020-08-01 11:38:05 -0300

    atualização 0108

---
+++ Exercícios_JS/Fundamentos/numberAlgunsCuidados.js
@@ -0,0 +1,5 @@
+console.log(7/0)
+console.log("10"/2) //interpreta essa string como valor numérico. com + ele não soma, mas concatena, porque a string tem prioridade. + é a única exceção
+console.log("Show!" * 2)
+console.log(0.1+0.7) //especificação do js não é tão precisa para ser mais rápida no processamento
+console.log((10).toFixed(2))
---
+++ Exercícios_JS/Fundamentos/templatestring.js
@@ -0,0 +1,11 @@
+const nome = 'Rebeca'
+const concatenacao = 'Olá ' + nome + '!'
+const template = `
+    Olá
+    ${nome}!` //template string é entre crases
+
+console.log(concatenacao,template)
+
+//expressões
+
+console.log(`1 + 1 = ${1+1}`)
